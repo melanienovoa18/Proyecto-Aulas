6f13480df18a374d29e890d06fc2a568ebf0615e
Yasmin Reyes <60162993+yasminreyes12@users.noreply.github.com>
2020-08-26 20:00:50 -0600

    Add files via upload

---
+++ UserObj.py
@@ -0,0 +1,12 @@
+class UserObj:
+    def __init__(
+        self, id, usuario, email, password,
+    ):
+        self.id = id
+        self.usuario = usuario
+        self.email = email
+        self.password = password
+
+    def getusuario(self):
+        usuario = self.usuario
+        return usuario
