a0f3cc06131fc486c126c954d10774efbf05ad50
Edgard-Rodriguez <60165865+Edgard-Rodriguez@users.noreply.github.com>
2020-08-27 15:51:26 -0600

    Update CardLogic.py

--- CardLogic.py
+++ CardLogic.py
@@ -1,24 +1,24 @@
 from Logic import Logic
 from CardObj import CardObj
 
+
 class CardLogic(Logic):
     def __init__(self):
         super().__init__()
         self.keys = [
-            "ID_pagos",
             "numero_tarjeta",
             "mes_vencimiento",
-            "anos_vencimiento",
+            "annos_vencimiento",
             "CCV",
         ]
     
     def insertCard(
-        self, ID_pagos, numero_tarjeta, mes_vencimiento, anos_vencimiento, CCV
+        self, numero_tarjeta, mes_vencimiento, annos_vencimiento, CCV
     ):
         database = self.get_databaseXObj()
         sql = (
-            "INSERT INTO reservas_evento.información_tarjeta (ID_pagos, numero_tarjeta, mes_vencimiento, años_vencimiento, CCV) "
-            + f"VALUES ('0', {numero_tarjeta}, {mes_vencimiento}, {anos_vencimiento}, {CCV});"
+            "INSERT INTO reservas_evento.informacion_tarjeta (ID_tarjeta, numero_tarjeta, mes_vencimiento, annos_vencimiento, CCV) "
+            + f"VALUES (0, {numero_tarjeta}, {mes_vencimiento}, {annos_vencimiento}, {CCV});"
         )
         answer = database.executeNonQueryBool(sql)
-        return answer
+        return answer 
