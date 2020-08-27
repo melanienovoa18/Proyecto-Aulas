6968c1d3413bd55819584eccc91c1544a41d9ad1
Edgard-Rodriguez <60165865+Edgard-Rodriguez@users.noreply.github.com>
2020-08-27 15:52:25 -0600

    Update CardObj.py

--- CardObj.py
+++ CardObj.py
@@ -1,16 +1,16 @@
 class CardObj:
     def _init_(
         self,
-        ID_pagos,
+        ID_tarjeta,
         numero_tarjeta,
         mes_vencimiento,
-        anos_vencimiento,
+        annos_vencimiento,
         CCV,
     ):
-        self.ID_pagos = ID_pagos
+        self.ID_tarjeta = ID_tarjeta
         self.numero_tarjeta = numero_tarjeta
         self.mes_vencimiento = mes_vencimiento
-        self.anos_vencimiento = anos_vencimiento
+        self.annos_vencimiento = annos_vencimiento
         self.CCV = CCV
 
     def getNumeroTarjeta(self):
